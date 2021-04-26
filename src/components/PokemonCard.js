5d5d06534764b0bc5ac71d584bcfecd9efe758f4
Jan Rubel <jasiekrubel@gmail.com>
2021-04-26 22:47:24 +0200

    Added pokemon type

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 import { Text, Surface } from 'react-native-paper';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import PokemonType from './PokemonType';
 
 export default function PokemonCard({ id, name, height, weight, sprite, types, color }) {
   return (
     <Surface style={styles.container}>
-      <Text style={styles.id}>{'#' + id}</Text>
+      <Text style={[styles.id, { color: 'black' }]}>{'#' + id}</Text>
       <MaterialCommunityIcons
         style={styles.favourite}
         name='heart-outline'
@@ -14,22 +15,30 @@ export default function PokemonCard({ id, name, height, weight, sprite, types, c
         color='black'
       />
       <Image style={styles.image} source={{ uri: sprite }} />
-      <Text style={{ color: 'black' }}>{name || 'asf'}</Text>
+      <Text style={[styles.name, { color: 'black' }]}>{name}</Text>
+      <View style={styles.typesContainer}>
+        {types.map((type) => (
+          <PokemonType type={type} passedStyle={{ marginHorizontal: 5 }} />
+        ))}
+      </View>
     </Surface>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 150,
-    width: 150,
+    height: 170,
+    width: 170,
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 10,
+    borderRadius: 12,
   },
   id: {
     position: 'absolute',
     left: 10,
     top: 10,
+    fontSize: 18,
   },
   favourite: {
     position: 'absolute',
@@ -37,7 +46,15 @@ const styles = StyleSheet.create({
     top: 10,
   },
   image: {
-    width: 50,
-    height: 50,
+    width: 80,
+    height: 80,
+  },
+  name: {
+    fontSize: 16,
+  },
+  typesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignContent: 'center',
   },
 });
--- src/components/PokemonType.js
+++ src/components/PokemonType.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text, useTheme } from 'react-native-paper';
 
-export default function PokemonType({ type }) {
+export default function PokemonType({ passedStyle, type }) {
+  const colors = useTheme().colors;
+  const typeColor = colors.pokemon.types[type];
   return (
-    <View>
-      <Text>{type}</Text>
+    <View style={[styles.container, { backgroundColor: typeColor }, passedStyle]}>
+      <Text style={{ fontSize: 12, color: colors.white }}>{type.toUpperCase()}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignContent: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 5,
+    paddingVertical: 2,
+    borderRadius: 10,
+  },
+});
