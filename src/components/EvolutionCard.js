7891c28587ea9b55522ac8d87dd1a75ca28c819c
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2021-05-26 19:25:14 +0200

    Implement evolution chain

---
+++ src/components/EvolutionCard.js
@@ -0,0 +1,116 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, ScrollView, View, Image } from 'react-native';
+import { useTheme, Text } from 'react-native-paper';
+import { pokemonByNameOrNumber } from '../contexts/PokeApiData';
+import CustomActivityIndicator from '../components/CustomActivityIndicator';
+import PokemonType from '../components/PokemonType';
+
+export default EvolutionCard = ({ color, id }) => {
+  const colors = useTheme().colors;
+  const isDarkTheme = useTheme().dark;
+  const pokemonColor = isDarkTheme ? colors.statBarBackground : colors.pokemon.background[color];
+  const activityIndicator = isDarkTheme
+    ? colors.activityIndicator
+    : colors.pokemon.background[color];
+
+  const [pokemon, setPokemon] = useState(null);
+  useEffect(() => {
+    pokemonByNameOrNumber(id).then((data) => {
+      setPokemon(data);
+    });
+  }, []);
+
+  return (
+    <>
+      {pokemon ? (
+        <View style={[styles.container, { backgroundColor: colors.card }]}>
+          <View style={[styles.pokemonContainer, { backgroundColor: pokemonColor }]}>
+            <Image style={styles.image} source={pokemon.sprite} />
+          </View>
+          <View style={styles.info}>
+            <Text style={styles.pokemonName}>{pokemon.name}</Text>
+            <Text style={styles.pokemonId}>{'#' + pokemon.id.toString().padStart(3, '0')}</Text>
+            <View style={styles.typeBox}>
+              {pokemon.types.map((type, index) => (
+                <PokemonType
+                  key={index}
+                  type={type}
+                  containerStyle={styles.typeContainer}
+                  textStyle={styles.typeText}
+                />
+              ))}
+            </View>
+          </View>
+        </View>
+      ) : (
+        <View style={[styles.tmpContainer, { backgroundColor: colors.card }]}>
+          <View style={{ width: '100%' }}>
+            <CustomActivityIndicator color={activityIndicator} />
+          </View>
+        </View>
+      )}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 'auto',
+    height: 164,
+    flexDirection: 'row',
+    borderRadius: 20,
+    elevation: 8,
+    marginTop: 16,
+    marginHorizontal: 8,
+  },
+  tmpContainer: {
+    width: 'auto',
+    height: 164,
+    flexDirection: 'row',
+    borderRadius: 20,
+    elevation: 8,
+    marginTop: 16,
+    marginHorizontal: 8,
+  },
+  pokemonContainer: {
+    height: '100%',
+    width: '50%',
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderTopRightRadius: 100,
+    borderBottomRightRadius: 100,
+  },
+  image: {
+    height: 132,
+    width: 132,
+    marginHorizontal: 16,
+    marginVertical: 16,
+  },
+  info: {
+    height: '100%',
+    width: '50%',
+  },
+  pokemonName: {
+    fontSize: 30,
+    textAlign: 'center',
+    paddingTop: 8,
+  },
+  pokemonId: {
+    fontSize: 20,
+    textAlign: 'center',
+    lineHeight: 20,
+  },
+  typeBox: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  typeContainer: {
+    width: '50%',
+    borderRadius: 100,
+  },
+  typeText: {
+    fontSize: 16,
+  },
+});
--- src/screens/details/Evolution.js
+++ src/screens/details/Evolution.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Dimensions, View, ScrollView } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
 import { DetailsAppBar } from '../../components';
+import EvolutionCard from '../../components/EvolutionCard';
 
 export default function Evolution({ color, sprite, chain }) {
   const colors = useTheme().colors;
+
+  var tier1 = chain;
+  var tier2 = chain.evolvesTo;
+  var tier3;
+
+  if (tier2.length == 1) {
+    tier3 = tier2[0].evolvesTo;
+  } else {
+    tier3 = [];
+  }
 
   return (
     <>
       <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
         <DetailsAppBar color={color} sprite={sprite} />
-        <Text style={{ color: colors.cardCaption }}>[Evolution]</Text>
+        <ScrollView style={styles.contentArea}>
+          <View style={{ paddingBottom: 16 }}>
+            <EvolutionCard color={color} id={tier1.pokemonID} />
+
+            {tier2.length == 1 && <EvolutionCard color={color} id={tier2[0].pokemonID} />}
+            {tier2.length > 1 && (
+              <ScrollView horizontal={true} pagingEnabled={true} persistentScrollbar={true}>
+                {tier2.map((it, i) => (
+                  <View
+                    key={i}
+                    style={{ width: Dimensions.get('window').width, paddingBottom: 16 }}
+                  >
+                    <EvolutionCard color={color} id={it.pokemonID} />
+                  </View>
+                ))}
+              </ScrollView>
+            )}
+
+            {tier3.length == 1 && <EvolutionCard color={color} id={tier3[0].pokemonID} />}
+            {tier3.length > 1 && (
+              <ScrollView horizontal={true} pagingEnabled={true} persistentScrollbar={true}>
+                {tier3.map((it, i) => (
+                  <View
+                    key={i}
+                    style={{ width: Dimensions.get('window').width, paddingBottom: 16 }}
+                  >
+                    <EvolutionCard color={color} id={it.pokemonID} />
+                  </View>
+                ))}
+              </ScrollView>
+            )}
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
@@ -21,5 +64,8 @@ const styles = StyleSheet.create({
   container: {
     height: '100%',
     alignItems: 'center',
+  },
+  contentArea: {
+    width: 'auto',
   },
 });
