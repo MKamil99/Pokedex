e9c2027a3f98a7a29622ee5a9820ee736937a64d
Jan Rubel <jasiekrubel@gmail.com>
2021-05-31 15:10:19 +0200

    Added info when there is no internet

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Text } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import './wdyr';
@@ -9,36 +11,60 @@ import {
   RobotoSlab_700Bold,
 } from '@expo-google-fonts/roboto-slab';
 import AppLoading from 'expo-app-loading';
+import NetInfo from '@react-native-community/netinfo';
 
 import { DetailsTabs, Filter, HomeTabs } from './src/screens';
-import { PokemonDataProvider } from './src/contexts';
-import { ThemeDataProvider } from './src/contexts/ThemeDataContext';
+import { PokemonDataProvider, ThemeDataProvider } from './src/contexts';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [isConnection, setIsConnection] = useState(false);
   const [fontsLoaded] = useFonts({
     RobotoSlab_400Regular,
     RobotoSlab_500Medium,
     RobotoSlab_700Bold,
   });
 
+  useEffect(() => {
+    NetInfo.addEventListener((state) => {
+      setIsConnection(state.isConnected);
+    });
+  }, []);
+
   return (
     <>
-      {fontsLoaded ? (
-        <PokemonDataProvider>
-          <ThemeDataProvider>
-            <NavigationContainer>
-              <Stack.Navigator headerMode='none' initialRouteName='Home'>
-                <Stack.Screen name='Home' component={HomeTabs} />
-                <Stack.Screen name='Details' component={DetailsTabs} />
-                <Stack.Screen name='Filter' component={Filter} />
-              </Stack.Navigator>
-            </NavigationContainer>
-          </ThemeDataProvider>
-        </PokemonDataProvider>
+      {isConnection ? (
+        <>
+          {fontsLoaded ? (
+            <PokemonDataProvider>
+              <ThemeDataProvider>
+                <NavigationContainer>
+                  <Stack.Navigator headerMode='none' initialRouteName='Home'>
+                    <Stack.Screen name='Home' component={HomeTabs} />
+                    <Stack.Screen name='Details' component={DetailsTabs} />
+                    <Stack.Screen name='Filter' component={Filter} />
+                  </Stack.Navigator>
+                </NavigationContainer>
+              </ThemeDataProvider>
+            </PokemonDataProvider>
+          ) : (
+            <AppLoading />
+          )}
+        </>
       ) : (
-        <AppLoading />
+        <SafeAreaView
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingHorizontal: '20%',
+          }}
+        >
+          <Text>
+            There is no internet connection. To use this app u need to connect with the internet.
+          </Text>
+        </SafeAreaView>
       )}
     </>
   );
