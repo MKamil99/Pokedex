cc0274256b2d17bbfd8ca5be6ab2100ec81abd54
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2021-06-02 09:31:50 +0200

    Landscape mode for general

--- src/screens/details/General.js
+++ src/screens/details/General.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 import { DetailsAppBar, PokemonGeneralInfo, PokemonStats } from '../../components';
+import GetOrientation from '../../utilities/GetOrientation';
+import IsPortrait from '../../utilities/IsPortrait';
 
 export default function General({ id, name, weight, height, stats, types, color, sprite }) {
   const colors = useTheme().colors;
+  const [currentStyle, setCurrentStyle] = useState(
+    IsPortrait(GetOrientation()) ? stylesPortrait : stylesLandscape
+  );
 
   const generalInfoProps = { id, name, types, weight, height };
 
+  useEffect(() => {
+    ScreenOrientation.addOrientationChangeListener(() => {
+      ScreenOrientation.getOrientationAsync().then((it) => {
+        setCurrentStyle(IsPortrait(it) ? stylesPortrait : stylesLandscape);
+      });
+    });
+  }, []);
+
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
+    <SafeAreaView style={[currentStyle.container, { backgroundColor: colors.background }]}>
       <DetailsAppBar color={color} sprite={sprite} />
       <ScrollView>
         <View style={{ paddingHorizontal: 8 }}>
@@ -22,9 +36,17 @@ export default function General({ id, name, weight, height, stats, types, color,
   );
 }
 
-const styles = StyleSheet.create({
+const stylesPortrait = StyleSheet.create({
   container: {
     height: '100%',
     width: '100%',
+  },
+});
+
+const stylesLandscape = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    flexDirection: 'row',
   },
 });
